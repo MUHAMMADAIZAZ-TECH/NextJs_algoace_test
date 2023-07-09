5d61e971d2be0d75e29adf81e24af7ca092b8644
MUHAMMADAIZAZ-TECH <aizaznadeem174@gmail.com>
2023-07-09 22:31:10 +0500

    resolve

--- pages/books/view.js
+++ pages/books/view.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { Card, Typography, Space, Rate } from 'antd';
+import { Card, Typography, Space, Rate } from "antd";
 
 const BookByID = () => {
-  const storage = JSON.parse(localStorage.getItem("book"))
-  const [book, setBook] = useState(storage);
+  const [book, setBook] = useState(null);
   const router = useRouter();
 
-  if (router.isFallback) {
+  useEffect(() => {
+    const storage = JSON.parse(localStorage.getItem("book"));
+    setBook(storage);
+  }, []);
+
+  if (router.isFallback || !book) {
     return <div>Loading...</div>;
   }
 
@@ -38,40 +42,57 @@ const BookByID = () => {
     display: "flex",
     justifyContent: "space-between",
     marginBottom: "10px",
-    marginLeft:'20px',
-    paddingLeft:'20px'
+    marginLeft: "20px",
+    paddingLeft: "20px"
   };
 
   return (
     <div style={containerStyle}>
       <div style={imageContainerStyle}>
         {book.imageLink && (
-          <img
-            src={book.imageLink}
-            alt="Book cover"
-            style={bookImageStyle}
-          />
+          <img src={book.imageLink} alt="Book cover" style={bookImageStyle} />
         )}
       </div>
       <div style={detailsContainerStyle}>
         <h2>{book.title}</h2>
         <div style={rowStyle}>
-          <p><strong>Rating</strong><br/>  <Rate disabled allowHalf defaultValue={book.rating} /></p>
-          <p style={{marginLeft:20}}><strong>Reviews</strong><br/> ({book.reviews})</p>
-          <p style={{marginLeft:20}}><strong>Price</strong><br/> ${book.price}</p>
+          <p>
+            <strong>Rating</strong>
+            <br /> <Rate disabled allowHalf defaultValue={book.rating} />
+          </p>
+          <p style={{ marginLeft: 20 }}>
+            <strong>Reviews</strong>
+            <br /> ({book.reviews})
+          </p>
+          <p style={{ marginLeft: 20 }}>
+            <strong>Price</strong>
+            <br /> ${book.price}
+          </p>
         </div>
-        <div style={{textAlign:'left'}}>
-          <p><strong>Author:</strong> {book.author}</p>
-          <p><strong>Country:</strong> {book.country}</p>
-          <p><strong>Language:</strong> {book.language}</p>
-          <p><strong>Pages:</strong> {book.pages}</p>
-          <button style={{
-            padding:15,
-            width:'100%',
-            border:'none',
-            backgroundColor: "#004D6D",
-            color:'white'
-          }} >View details</button>
+        <div style={{ textAlign: "left" }}>
+          <p>
+            <strong>Author:</strong> {book.author}
+          </p>
+          <p>
+            <strong>Country:</strong> {book.country}
+          </p>
+          <p>
+            <strong>Language:</strong> {book.language}
+          </p>
+          <p>
+            <strong>Pages:</strong> {book.pages}
+          </p>
+          <button
+            style={{
+              padding: 15,
+              width: "100%",
+              border: "none",
+              backgroundColor: "#004D6D",
+              color: "white"
+            }}
+          >
+            View details
+          </button>
         </div>
       </div>
     </div>
